716eaa42bf609ae9bba387b201d34b66f0380c8d
Matt Schwartz <matt.schwartz@verafi.net>
2024-02-07 15:55:08 -0700

    Update model specs

--- spec/models/cocktail_spec.rb
+++ spec/models/cocktail_spec.rb
@@ -2,38 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Cocktail, type: :model do
   describe "validations" do
+    let!(:cocktail) { FactoryBot.create(:cocktail, name: "Vodka Tonic") }
+
     it "is valid with valid attributes" do
-      expect(FactoryBot.create(:cocktail)).to be_valid
+      expect(cocktail).to be_valid
     end
 
     it "validates presence of name" do
-      cocktail = FactoryBot.create(:cocktail)
       cocktail.name = ""
       expect(cocktail).not_to be_valid
     end
 
     it "validates presence of category" do
-      cocktail = FactoryBot.create(:cocktail)
       cocktail.category = ""
       expect(cocktail).not_to be_valid
     end
 
     it "validates presence of container" do
-      cocktail = FactoryBot.create(:cocktail)
       cocktail.container = ""
       expect(cocktail).not_to be_valid
     end
 
     it "validates presence of instructions" do
-      cocktail = FactoryBot.create(:cocktail)
       cocktail.instructions = ""
       expect(cocktail).not_to be_valid
     end
 
-    it "validates presence ofimage" do
-      cocktail = FactoryBot.create(:cocktail)
+    it "validates presence of image" do
       cocktail.image = ""
       expect(cocktail).not_to be_valid
+    end
+
+    it "validates uniqueness of name" do
+      cocktail2 = FactoryBot.build(:cocktail, name: "Vodka Tonic")
+      expect(cocktail2).not_to be_valid
     end
   end
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,9 +2,10 @@
 
 RSpec.describe Ingredient, type: :model do
   describe "validations" do
+    let!(:cocktail) { FactoryBot.create(:cocktail, name: "Vodka Tonic") }
+    let!(:ingredient) { FactoryBot.create(:ingredient, name: "Lemon peel", cocktail: cocktail) }
+
     it "validates uniqueness of name" do
-      cocktail = FactoryBot.create(:cocktail)
-      ingredient1 = FactoryBot.create(:ingredient, name: "Lemon peel", cocktail: cocktail)
       ingredient2 = FactoryBot.build(:ingredient, name: "Lemon peel", cocktail: cocktail)
       expect(ingredient2).not_to be_valid
     end
