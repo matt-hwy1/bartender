d50d119280e4693b037ebfa5ba2aa73ba0a9b523
Matt Schwartz <matt.schwartz@verafi.net>
2024-02-07 12:44:52 -0700

    Render Cocktail model JSON directly

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::CocktailsController < ApplicationController
   def index
     if params[:q].present? && params[:q].length >= 3
-      render json: Cocktail.includes(:ingredients).where("name LIKE ?", "%#{params[:q]}%")
+      render json: { cocktails: Cocktail.includes(:ingredients).where("name LIKE ?", "%#{params[:q]}%") }
     else
-      render json: []
+      render json: { error: "The cocktail search parameter must be at least 3 characters long"}
     end
   end
 
   def show
-    render json: Cocktail.includes(:ingredients).find(params[:id])
+    render json: { cocktail: Cocktail.includes(:ingredients).find(params[:id]).to_api_json }
   end
 end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,8 @@
 class Cocktail < ApplicationRecord
   has_many :ingredients, dependent: :destroy
+
+  def as_json(options = nil)
+    super({ only: [:id, :name, :category, :container, :instructions, :image],
+            include: { ingredients: { only: [:name, :measurement] } } }.merge(options || {}))
+  end
 end
