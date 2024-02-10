32b0c30fe2ff9f3ed2a9a51e6ed87a9249d59e65
Matt Schwartz <matt.schwartz@verafi.net>
2024-02-09 22:47:06 -0700

    Refactor controller for simplicity

--- app/controllers/api/cocktails_controller.rb
+++ app/controllers/api/cocktails_controller.rb
@@ -6,16 +6,25 @@ class Api::CocktailsController < ApplicationController
   # Implement full-text searching in the database and queries if it grows in size
 
   def index
-    if params[:query].present? && params[:query].length >= 2
-      render json: { drinks: Cocktail.search(params[:query],
-                                params[:offset] || Cocktail::OFFSET, params[:limit] || Cocktail::PAGE_SIZE) }
-    else
-      render json: []
-    end
+    render json: { drinks: Cocktail.search(query, offset, limit) }
   end
 
   def show
     # Using where here instead of find in order to return an empty array in case the record is not found
     render json: { drinks: Cocktail.includes(:ingredients).where(id: params[:id]) }
+  end
+
+  private
+
+  def query
+    params[:query]
+  end
+
+  def offset
+    params[:offset] || Cocktail::OFFSET
+  end
+
+  def limit
+    params[:limit] || Cocktail::PAGE_SIZE
   end
 end
